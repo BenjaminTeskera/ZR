2d1cb8c4fbfe46939f5f4fa24fcb334b7169822b
Benjamin Teskera <benjamin.teskera@fer.unizg.hr>
2025-06-22 21:51:08 +0200

    Add compression corrections

--- Wavelet_tree.cpp
+++ Wavelet_tree.cpp
@@ -28,8 +28,6 @@ public:
     vector<int> C_ni;
     vector<int> select0B;
     vector<int> select1B;
-    //vector<int>* leftS;
-    //vector<int>* rightS;
     Node(vector<int>* alphabet, vector<int>* S, int S_size, Node* parent, int l_ni, int r_ni, int depth)
         : alphabet(alphabet), S(S), parent(parent), l_ni(l_ni), r_ni(r_ni), depth(depth) {
         //std::chrono::steady_clock::time_point beg = std::chrono::high_resolution_clock::now();
@@ -59,9 +57,6 @@ public:
         select1B.resize(num_of_1);
         vector<int> leftS; leftS.resize(num_of_0);
         vector<int> rightS; rightS.resize(num_of_1);
-        //std::chrono::steady_clock::time_point end = std::chrono::high_resolution_clock::now();
-        //cout << "   Time taken for memory allocation: " << std::chrono::duration_cast<microseconds>(end - beg).count() << endl;
-        //beg = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < S_size; i++) {
             a = S->at(i);
             value = values.at(a);
@@ -78,10 +73,49 @@ public:
                 select1B[pos1++] = (i + 1); //zbog indeksiranja od 1
             }
         }
-        //end = std::chrono::high_resolution_clock::now();
-        //cout << "   Time taken for filling data structures: " << std::chrono::duration_cast<microseconds>(end - beg).count() << endl;
         u_child = new Node(alphabet, &leftS, pos0, this, l_ni, m_ni, depth + 1);
         w_child = new Node(alphabet, &rightS, pos1, this, m_ni + 1, r_ni, depth + 1);
+    }
+    Node(vector<int>* alphabet, vector<int>* S, int S_size, Node* parent, int l_ni, int r_ni, int depth, bool compression) 
+        : alphabet(alphabet), S(S), parent(parent), l_ni(l_ni), r_ni(r_ni), depth(depth) {
+        if (l_ni == r_ni) {
+            return;
+        }
+        m_ni = (l_ni + r_ni) / 2;
+        int a, value;
+        int num_of_0 = 0;
+        int num_of_1 = 0;
+        int pos0 = 0;
+        int pos1 = 0;
+        for (int i = 0; i < S_size; i++) {
+            a = S->at(i);
+            value = values.at(a);
+            if (value <= m_ni) {
+                num_of_0++;
+            }
+            else {
+                num_of_1++;
+            }
+        }
+        B_ni.resize(S_size);
+        vector<int> *leftS = new vector<int>(); leftS->resize(num_of_0);
+        vector<int> *rightS = new vector<int>(); rightS->resize(num_of_1);
+        for (int i = 0; i < S_size; i++) {
+            a = S->at(i);
+            value = values.at(a);
+            if (value <= m_ni) {
+                (*leftS)[pos0++] = a;
+                B_ni[i] = (false);
+            }
+            else {
+                (*rightS)[pos1++] = a;
+                B_ni[i] = (true);
+            }
+        }
+        u_child = new Node(alphabet, leftS, pos0, this, l_ni, m_ni, depth + 1, true);
+        delete leftS;
+        w_child = new Node(alphabet, rightS, pos1, this, m_ni + 1, r_ni, depth + 1, true);
+        delete rightS;
     }
     ~Node() {
         delete u_child;
@@ -91,13 +125,13 @@ public:
         if (l_ni == r_ni) {
             return    4 + 4 + 4 + 4 //int l_ni, int r_ni, int m_ni, int depth
                 + 8 + 8 + 8 //Node* u_child, Node* w_child i Node* parent
-                + 8 + 8 //vector<int>* S i vector<int>* alphabet
+                + 16 + 16 //vector<int>* S i vector<int>* alphabet
                 + 32 * 4 //prazni vektori B_ni, C_ni, select0B i select1B
                 + 24 * 2;
         }
         return    4 + 4 + 4 + 4 //int l_ni, int r_ni, int m_ni, int depth
             + 8 + 8 + 8 //Node* u_child, Node* w_child i Node* parent
-            + 8 + 8 //vector<int>* S i vector<int>* alphabet
+            + 16 + 16 //vector<int>* S i vector<int>* alphabet
             + (B_ni.size() + 7) / 8
             + C_ni.size() * sizeof(int)
             + select0B.size() * sizeof(int) * 2 //za select0B i leftS
@@ -127,6 +161,22 @@ public:
         cout << std::endl;
         if (u_child != nullptr) u_child->print_recursion("left");
         if (w_child != nullptr) w_child->print_recursion("right");
+    }
+    int accessCompressed(int i) {
+        if (i == 0) return NULL;
+        if (l_ni == r_ni) {
+            return alphabet->at(l_ni);
+        }
+        if (!B_ni[i - 1]) {
+            i = 0;
+            for (bool bit : B_ni) bit == 0 ? i++ : 0;
+            return u_child->accessCompressed(i);
+        }
+        else {
+            i = 0;
+            for (bool bit : B_ni) bit == 1 ? i++ : 0;
+            return w_child->accessCompressed(i);
+        }
     }
     int access(int i) {
         if (i == 0) return NULL;
@@ -224,16 +274,29 @@ public:
 class Tree {
 public:
     int value;
+    vector<int> *alphabet;
     Node* root = nullptr;
-    Tree(vector<int>* txt, vector<int>* alphabet) {
+    Tree(vector<int>* txt, vector<int>* alphabet_p) {
         values.clear();
         for (int i = 0; i < alphabet->size(); i++) {
+            alphabet = new vector<int>(*alphabet_p);
             values[alphabet->at(i)] = i;
         }
         root = new Node(alphabet, txt, txt->size(), nullptr, 0, (alphabet->size()) - 1, 1);
     }
+    Tree(vector<int>* txt, vector<int>* alphabet_p, bool compression) {
+        alphabet = new vector<int>(*alphabet_p);
+        values.clear();
+        for (int i = 0; i < alphabet->size(); i++) {
+            values[alphabet->at(i)] = i;
+        }
+        root = new Node(alphabet, txt, txt->size(), nullptr, 0, (alphabet->size()) - 1, 1, true);
+    }
     int tree_memory() {
         return 4 + 24 + root->tree_memory();
+    }
+    int accessCompressed(int i) {
+        return root->accessCompressed(i);
     }
     int access(int i) {
         return root->access(i);
@@ -245,14 +308,22 @@ public:
         return root->select(a, j);
     }
     ~Tree() {
+        delete alphabet;
         delete root;
     }
 };
+bool contains(vector<int>* s, int a) {
+    for (int b: *s) {
+        if(b == a) return true;
+    }
+    return false;
+}
 int main(void) {
-    vector<int> alphabet;
-    vector<int> txt;
-    std::chrono::steady_clock::time_point beg;
-    std::chrono::steady_clock::time_point end;
+    vector<int> *alphabet = new vector<int>();
+    vector<int> *txt = new vector<int>();
+    //std::chrono::steady_clock::time_point beg;
+    //std::chrono::steady_clock::time_point end;
+    /*
     int sigma;
     int txt_size;
     int result = 0;
@@ -266,9 +337,9 @@ int main(void) {
     int select_op_counter = 0;
     int memory = 0;
     cout << "text_size,sigma,tree_construction_time,values_map_construction_time,avg_access_time,avg_rank_time,avg_select_time,memory" << endl;
-    for (int sigma_magnitude = 1; sigma_magnitude < 2; sigma_magnitude++) { //11
-        for (int txt_magnitude = 10; txt_magnitude < 11; txt_magnitude++) { //25 za bazu 2 //txt_magnitude++
-            int num_of_runs = 3;
+    for (int sigma_magnitude = 1; sigma_magnitude < 20; sigma_magnitude++) { //11
+        for (int txt_magnitude = 1000000; txt_magnitude < 2000001; txt_magnitude += 1000000) { //25 za bazu 2 //txt_magnitude++
+            int num_of_runs = 1;
             result = 0;
             sum_access_time = 0;
             sum_rank_time = 0;
@@ -284,13 +355,12 @@ int main(void) {
             sigma = pow(2, sigma_magnitude);
 
             txt_size = txt_magnitude;
-            /*
-            if (txt_magnitude < 1000000) {
-                txt_magnitude += 100000;
-            } else {
-                txt_magnitude += 250000;
-            }
-            */
+            //if (txt_magnitude < 1000000) {
+            //   txt_magnitude += 100000;
+            //} else {
+            //    txt_magnitude += 250000;
+            //}
+
             //txt_size = pow(2, txt_magnitude);
             for (int run = 1; run <= num_of_runs; run++) {
                 txt.clear();
@@ -368,4 +438,121 @@ int main(void) {
                 << endl;
         }
     }
+    */
+    string a;
+    ifstream myfile("C:\\Users\\Benjamin\\Desktop\\Desktop mape\\skooool i fakultet\\faks\\6. semestar\\ZR\\message(6).txt");
+    txt->resize(2110968); //1696601 //4411532 //13588 //6552 //4641652 //2110968 //size of whatever is suppoesd to be conpressed
+    int i = 0;
+    if (myfile.is_open()) {
+        bool seen = false;
+        while (getline(myfile, a)) {
+            for (char b : a) {
+                //if (b == 32) continue;
+                if (!contains(alphabet, b)) alphabet->push_back(b);
+                //txt->push_back(b);
+                (*txt)[i++] = (int)b;
+            }
+        }
+        myfile.close();
+    }
+    else {
+        return -1;
+    }
+    //alphabet->push_back('A'); alphabet->push_back('C'); alphabet->push_back('G'); alphabet->push_back('T');
+    copy(alphabet->begin(), alphabet->end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+    cout << txt->size() << endl;
+    //copy(txt.begin(), txt.end(), ostream_iterator<int>(cout, " "));
+    Tree* t = new Tree(txt, alphabet, true);
+    delete txt; delete alphabet;
+    cout << t->tree_memory();
+    cout << "amogus";
+    delete t;
+    cout << "sus";
+    /*
+    vector<int> alphabet;
+    vector<int> txt;
+    int sigma;
+    int txt_size;
+    int result = 0;
+    int values_construction_time = 0;
+    int tree_construction_time = 0;
+    int sum_access_time = 0;
+    int sum_rank_time = 0;
+    int sum_select_time = 0;
+    int access_op_counter = 0;
+    int rank_op_counter = 0;
+    int select_op_counter = 0;
+    int memory = 0;
+    for (int sigma_magnitude = 2; sigma_magnitude < 3; sigma_magnitude++) { //11
+        for (int txt_magnitude = 1; txt_magnitude < 9; txt_magnitude ++) { //25 za bazu 2 //txt_magnitude++
+            int num_of_runs = 1;
+            result = 0;
+            sum_access_time = 0;
+            sum_rank_time = 0;
+            sum_select_time = 0;
+            access_op_counter = 0;
+            rank_op_counter = 0;
+            select_op_counter = 0;
+            memory = 0;
+            sigma = pow(2, sigma_magnitude);
+            //txt_size = txt_magnitude;
+            txt_size = pow(10, txt_magnitude);
+            for (int run = 1; run <= num_of_runs; run++) {
+                txt.clear();
+                alphabet.clear();
+                values.clear();
+                num_of_occurances.clear();
+
+                //cout << txt_size << "," << sigma << ",";
+                for (int i = 0; i < sigma; i++) {
+                    alphabet.push_back(i);
+                    num_of_occurances[i] = 0;
+                }
+                //Stvaranje nasumiènog teksta za testiranje performanse
+                int a;
+                for (int i = 0; i < txt_size; i++) {
+                    a = (rand() % sigma);
+                    num_of_occurances[a]++;
+                    txt.push_back(a);
+                }
+                //cout << endl;
+                //copy(txt.begin(), txt.end(), ostream_iterator<int>(cout, ",")); cout << endl;
+                //Stvaranje strukture s svim znakovima abecede i njihovim vrijednostima.
+                beg = high_resolution_clock::now();
+                int rows = ceil(log2(alphabet.size()));
+                for (int i = 0; i < alphabet.size(); i++) {
+                    values[alphabet.at(i)] = i;
+                }
+                end = high_resolution_clock::now();
+                values_construction_time += duration_cast<microseconds>(end - beg).count();
+                beg = high_resolution_clock::now();
+                Tree* t = new Tree(&txt, &alphabet, true);
+                end = high_resolution_clock::now();
+                tree_construction_time += duration_cast<microseconds>(end - beg).count();
+                memory += t->tree_memory();
+                //m->print();
+                for (int j = 0; j < txt_size + 1; j++) {
+                    beg = std::chrono::high_resolution_clock::now();
+                    result = t->root->accessCompressed(j);
+                    end = std::chrono::high_resolution_clock::now();
+                    sum_access_time += std::chrono::duration_cast<microseconds>(end - beg).count();
+                    access_op_counter++;
+                    //cout << "access(" << j << ")," << result << "," << std::chrono::duration_cast<microseconds>(end - beg).count() << endl;
+                    if (access_op_counter % 100 == 0) break;
+                }
+                delete(t);
+                cout << txt_size << endl;
+            }
+            cout << txt_size << ","
+                << sigma << ","
+                << ((tree_construction_time - values_construction_time) / num_of_runs) << ","
+                << (values_construction_time / num_of_runs) << ","
+                << ((sum_access_time / access_op_counter)) << ","
+                << (memory / num_of_runs)
+                << endl;
+        }
+    }
+    */
+    return 0;
 }
